0c6f2a77fd0c2f0a085f1f6200af5ee8ca1995f4
Conrado Rodriguez <conrado.rgz@gmail.com>
2023-10-03 08:31:12 -0600

    Improve immutability

--- src/test/java/com/github/crgz/utils/CrookedDie.java
+++ src/test/java/com/github/crgz/utils/CrookedDie.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 import static com.github.crgz.GameTest.EXPECTED_OUTPUT;
 
 public class CrookedDie implements Die {
-    protected Iterator<Integer> iterator = Matches.get("^Die result: ([0-6]).*", EXPECTED_OUTPUT).stream()
+    protected final Iterator<Integer> iterator = Matches.get("^Die result: ([0-6]).*", EXPECTED_OUTPUT).stream()
             .map(Integer::valueOf)
             .toList()
             .iterator();
